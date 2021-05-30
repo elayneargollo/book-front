3d0dcb5c6f5f9bce793950da9c8a921aab365337
elayne <elayne.argollo@solutis.com.br>
2021-05-30 17:50:23 -0300

    tela de edicao de usuario

--- src/pages/user/User.js
+++ src/pages/user/User.js
@@ -9,15 +9,21 @@ import TableRow from '@material-ui/core/TableRow';
 import Paper from '@material-ui/core/Paper';
 import swal from 'sweetalert';
 import CircularProgress from '@material-ui/core/CircularProgress';
-import './Sytle.css' 
+import './Sytle.css'
+import IconButton from '@material-ui/core/IconButton';
+import DeleteIcon from '@material-ui/icons/Delete';
+import EditIcon from '@material-ui/icons/Edit';
+import { Button } from '@material-ui/core';
+import { useHistory } from 'react-router-dom'
+import { cadastro } from '../../routes/paths';
+import { deleteById } from "../../services/api/login";
 
-export default function DenseTable() {
+export default function User() {
 
   const classes = useStyles();
-
   const [users, setUsers] = React.useState({ "empty": "true", "dir": [] });
-
   const [loading, setLoading] = useState(true);
+  let history = useHistory();
 
   useEffect(() => {
     fetchData();
@@ -36,6 +42,25 @@ export default function DenseTable() {
       });
   };
 
+  const register = () => {
+    history.replace(cadastro); 
+  }
+
+  const deleteUser = (id) => {
+    async function loadBookId() {
+      const response = await deleteById(id);
+      swal("", response, "success");
+    }
+    loadBookId();
+    refreshPage();
+  }
+
+  function refreshPage() {
+    setTimeout(()=>{
+        window.location.reload(false);
+    }, 1000);
+}
+
 
   if (loading) {
     return (
@@ -46,32 +71,59 @@ export default function DenseTable() {
   } else {
 
     return (
-      <div className='espacamento'>
-        <TableContainer component={Paper}>
-          <Table className={classes.table} size="small" aria-label="a dense table">
-            <TableHead>
-              <TableRow >
-                <StyledTableCell>Username</StyledTableCell>
-                <StyledTableCell align="right">Matricula</StyledTableCell>
-                <StyledTableCell align="right">Password</StyledTableCell>
-                <StyledTableCell align="right">Role</StyledTableCell>
-              </TableRow>
-            </TableHead>
-            <TableBody>
-              {users.map((row) => (
-                <StyledTableRow key={row.username}>
-                  <TableCell component="th" scope="row">
-                    {row.username}
-                  </TableCell>
-                  <TableCell align="right">{row.id}</TableCell>
-                  <TableCell align="right">{row.password}</TableCell>
-                  <TableCell align="right">{row.role}</TableCell>
-                </StyledTableRow>
-              ))}
-            </TableBody>
-          </Table>
-        </TableContainer>
+      <div>
+        <div className='espacamento'>
+          <h1>Registered Users</h1>
+          <TableContainer component={Paper}>
+            <Table className={classes.table} size="small" aria-label="a dense table">
+              <TableHead>
+                <TableRow >
+                  <StyledTableCell>Username</StyledTableCell>
+                  <StyledTableCell align="right">Registration</StyledTableCell>
+                  <StyledTableCell align="right">Password</StyledTableCell>
+                  <StyledTableCell align="right">Role</StyledTableCell>
+                  <StyledTableCell align="right">Actions</StyledTableCell>
+                </TableRow>
+              </TableHead>
+              <TableBody>
+                {users.map((row) => (
+                  <StyledTableRow key={row.username}>
+                    <TableCell component="th" scope="row">
+                      {row.username}
+                    </TableCell>
+                    <TableCell align="right">{row.id}</TableCell>
+                    <TableCell align="right">{row.password}</TableCell>
+                    <TableCell align="right">{row.role}</TableCell>
+                    <TableCell align="right">
+                      <IconButton
+                        aria-label="delete"
+                        onClick={() => deleteUser(row.id)}
+                        title="delete user">
+                        <DeleteIcon />
+                      </IconButton>
+                      <IconButton
+                        aria-label="edit"
+                        title="edit user">
+                        <EditIcon />
+                      </IconButton>
+                    </TableCell>
+                  </StyledTableRow>
+                ))}
+              </TableBody>
+            </Table>
+          </TableContainer>
+        </div>
+        <div className='novoUser'>
+          <Button 
+          size="small" 
+          variant="contained" 
+          color="primary"
+          onClick={() => register()}>
+          New user
+          </Button>
+        </div>
       </div>
+
     );
   }
 }
